26fc4dbbab83da7f125d431d73fd44ecfb7716eb
henrymutega <hmutega@gmail.com>
2019-06-20 14:21:35 +0300

    Add toggle the selected images.

--- js/scripts.js
+++ js/scripts.js
@@ -10,42 +10,44 @@ $("#submit").click(function(){
   document.getElementById("loc").innerHTML =
   (location.options[location.selectedIndex].text).toUpperCase();
 
-  y = model.options[model.selectedIndex].text;
-  x = location.options[location.selectedIndex].text;
+var  y = model.options[model.selectedIndex].text;
+var  x = location.options[location.selectedIndex].text;
 
-  if(y=="Nissan" || x=="Thika Road"){
-    $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
+  if(x =="Ngong Road"){
+    $("#jane, #adams, #ken, #bill, #caka, #peter, #eduwin, #sheila, #mwangi, #yussuf, #mwanza, #mercy").hide();
+    $("#oscar").show();
     $("#john").show();
+  }else if(x=="Thika Road"){
+    $("#oscar, #ken, #bill, #caka, #peter, #eduwin, #sheila, #mwangi, #yussuf, #mwanza, #mercy").hide();
     $("#jane").show();
+    $("#adams").show();
+  }else if(x=="Kiambu Road"){
+    $("#oscar, #john, #jane, #adams, #caka, #peter, #eduwin, #sheila, #mwangi, #yussuf, #mwanza, #mercy").hide();
     $("#ken").show();
-  }else if(y=="Toyota"){
-    $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
-    $("#oscar").show();
-    $("#adams").show();
-  }else if(y=="Range Rover"){
-    $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
-    $("#oscar").show();
-    $("#adams").show();
-    $("#caka").show();
-  }else if(y=="BMW"){
-    $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
-    $("#oscar").show();
-  }else if(y=="Volvo") {
-    $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
-    $("#john").show();
-    $("#jane").show();
     $("#bill").show();
-  }else if(y=="Ford") {
-    $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
-    $("#oscar").show();
-    $("#jane").show();
+  }else if(x=="Eastlands"){
+    $("#oscar, #john, #jane, #adams, #ken, #bill, #eduwin, #sheila, #mwangi, #yussuf, #mwanza, #mercy").hide();
+    $("#caka").show();
+    $("#peter").show();
+  }else if(x=="Westlands") {
+    $("#oscar, #john, #jane, #adams, #ken, #bill, #caka, #peter, #eduwin, #mwangi, #yussuf, #mwanza, #mercy").hide();
+    $("#eduwin").show();
+    $("#sheila").show();
+  }else if(x=="Jogoo Road") {
+    $("#oscar, #john, #jane, #adams, #ken, #bill, #caka, #peter, #eduwin, #sheila, #mwanza, #mercy").hide();
+    $("#mwangi").show();
+    $("#yussuf").show();
+  }else if(x=="Nairobi CBD") {
+    $("#oscar, #john, #jane, #adams, #ken, #bill, #caka, #peter, #eduwin, #sheila").hide();
+    $("#mwanza").show();
+    $("#mercy").show();
   }else if(y=="--Select Your Car Model--" || x=="--Select Your Location--"){
     $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").hide();
-    alert("Enter your Car model and location to Find our Mechanics");
+    alert("Please Enter your Car model and location to Find our Mechanics.");
   }
 })
 
 $("#display").click(function(){
-  $("#ken, #jane, #oscar, #adams, #caka, #bill, #john").toggle();
+  $("#oscar, #john, #jane, #adams, #ken, #bill, #caka, #peter, #eduwin, #sheila, #mwangi, #yussuf, #mwanza, #mercy").toggle();
 })
 });
